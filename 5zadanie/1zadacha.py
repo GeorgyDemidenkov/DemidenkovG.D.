fa328928004e778038801756b2c14b52fc8c1dbe
GeorgyDemidenkov <98282737+GeorgyDemidenkov@users.noreply.github.com>
2023-10-29 21:03:01 +0300

    Update 1zadacha.py

--- 5zadanie/1zadacha.py
+++ 5zadanie/1zadacha.py
@@ -1,9 +1,6 @@
-def kef(A,B):
-  if A<=B:
-  for i in range(A,B+1):
-    print(i)
-  else:
-    print("A нужно < или = B")
-A=int(input()) 
-B=int(input()) 
-kef(A,B)
+def keft(N):
+    for i in range(1,N):
+        a=i*i
+        print(a)
+N=int(input())
+keft(N)
