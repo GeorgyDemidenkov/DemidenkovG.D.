0a6861ba7af690e78adcd6fd935795f3d2a84348
GeorgyDemidenkov <98282737+GeorgyDemidenkov@users.noreply.github.com>
2023-10-31 02:11:02 +0300

    Create 7zadacha.py

---
+++ 5zadanie/7zadacha.py
@@ -0,0 +1,12 @@
+def farq(n):
+    a=0
+    previous=None
+    while True:
+        if n==0:
+            break  
+        if previous is not None and n>previous:
+            a+=1
+        previous=n
+    print(a)
+n=int(input())
+farq(n)
