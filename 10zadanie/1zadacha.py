d01547f2c4d4e438fd45a91e24355890a37d132c
GeorgyDemidenkov <98282737+GeorgyDemidenkov@users.noreply.github.com>
2023-12-03 15:33:42 +0300

    Create 1zadacha.py

---
+++ 10zadanie/1zadacha.py
@@ -0,0 +1,29 @@
+def find(matrix):
+    maxelrow = []
+    minelcolumn = []
+    for row in matrix:
+        maxel = max(row)
+        maxelrow.append(maxel)
+    transposed_matrix = list(zip(*matrix))
+    for column in transposed_matrix:
+        minel = min(column)
+        minelcolumn.append(minel)
+    return maxelrow, minelcolumn
+
+def readfile(file_name):
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        matrix = [list(map(int, line.split())) for line in lines]
+    return matrix
+
+def writefile(file_name, maxelrow, minelcolumn):
+    with open(file_name, 'w') as file:
+        file.write(f"Наибольшие элементы в строках: {maxelrow}\n")
+        file.write(f"Наименьшие элементы в столбцах: {minelcolumn}\n")
+
+input_file_name = "ДемиденковГеоргийДмитриевич_У-234_vvod.txt"
+output_file_name = "ДемиденковГеоргийДмитриевич_У-234_vivod.txt"
+
+re = readfile(input_file_name)
+maxelrow, minelcolumn = find(re)
+writefile(output_file_name, maxelrow, minelcolumn)
